938a54379c46682c970e3a2da4b96a7bdf1979e0
Sasha <mukhych@mail.ru>
2026-07-06 13:37:32 +0500

    first push

---
+++ count_game.py
@@ -0,0 +1,8 @@
+from typing import TypedDict
+
+
+class Count(TypedDict):
+    count_player: int
+    capacity_bunker: int
+    message: str
+    round: int
---
+++ user.py
@@ -0,0 +1,11 @@
+from typing import TypedDict
+
+
+class UserPlayer(TypedDict):
+    profession: str
+    biology: str
+    health: str
+    hobby: str
+    baggage: str
+    fact: str
+    
